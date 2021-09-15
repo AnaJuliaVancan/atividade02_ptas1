829bc5aa469bf6278ac2b7e5a310d413043c3b2d
Ana Júlia Júlia <>
2021-09-15 18:19:33 +0000

    index.html removido

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.set ('view engine' , 'ejs');
+app.engine ('html', require('ejs').renderFile);
+
 app.get('/', (req, res) => {
   res.send("Minha primeira página");
-})
-
-app.get('/blog', (req, res) => {
-  res.send("Minha primeira página do blog");
-})
+});
 
 app.get('/contatos', (req, res) => {
   res.send("Meu contato é anajuliavancan@gmail.com");
-})
+});
 
 app.listen(port, () => {
   console.log('O servidor está funcionado xuxu beleza');
-})
+});
