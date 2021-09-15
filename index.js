8be62cc95fe40798e83940189a6bdcc4455e8015
Ana Júlia Júlia <85965741+AnaJuliaVancan@users.noreply.github.com>
2021-09-15 18:30:44 +0000

    index.ejs adicionado

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.set ('view engine' , 'ejs');
-app.engine ('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
   res.send("Minha primeira página");
